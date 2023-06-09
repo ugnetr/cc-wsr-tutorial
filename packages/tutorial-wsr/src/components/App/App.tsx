f6e6dc294c8274c8b301e35e7b048c8aca373d74
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 12:50:29 +0300

    Add layout inside a card

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -1,52 +1,19 @@
-import React from "react";
-import { Layout, Cell, Card } from "wix-style-react";
+import React from 'react';
+import { Layout, Cell, Card } from 'wix-style-react';
 
 export default () => (
   <Layout>
     <Cell span={6}>
       <Card>
-        <Card.Header title="first card" subtitle="nice" />
-        <Card.Divider />
-        <Card.Content>some content</Card.Content>
-      </Card>
-    </Cell>
-    <Cell span={6}>
-      <Card>
-        <Card.Header title="What is Lorem Ipsum?" />
-        <Card.Divider />
+        <Card.Header title="first card" />
+        <Card.Divider/>
         <Card.Content>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          <Layout>
+            <Cell span={4}>first part</Cell>
+            <Cell span={4}>second part</Cell>
+            <Cell span={4}>third part</Cell>
+          </Layout>
         </Card.Content>
-      </Card>
-    </Cell>
-
-    <Cell span={4}>
-      <Card stretchVertically={true}>
-        <Card.Header title="First card" />
-        <Card.Divider />
-        <Card.Content>Some content.</Card.Content>
-      </Card>
-    </Cell>
-    <Cell span={4}>
-      <Card>
-        <Card.Header title="Second card" subtitle="cool" />
-        <Card.Divider />
-        <Card.Content>Some content.</Card.Content>
-      </Card>
-    </Cell>
-    <Cell span={4}>
-      <Card>
-        <Card.Header title="Third card" subtitle="nice" />
-        <Card.Divider />
-        <Card.Content>Some content.</Card.Content>
       </Card>
     </Cell>
   </Layout>
