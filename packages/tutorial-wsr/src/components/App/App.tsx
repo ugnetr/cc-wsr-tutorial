3c9c944f562f5d27b33453bb89f8a3e2b93b2fec
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 12:42:28 +0300

    Add nested layout

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -3,11 +3,11 @@ import { Layout, Cell, Box } from 'wix-style-react';
 
 export default () => (
   <Layout>
-    <Cell><Box border="1px solid black">I am a full row</Box></Cell>
-    <Cell span={6}><Box border="1px solid black">I take half of the size</Box></Cell>
-    <Cell span={6}><Box border="1px solid black">me too</Box></Cell>
-    <Cell span={4}><Box border="1px solid black">third</Box></Cell>
-    <Cell span={4}><Box border="1px solid black">me too</Box></Cell>
-    <Cell span={4}><Box border="1px solid black">me too</Box></Cell>
+    <Cell span={6}>
+      <Layout>
+        <Cell span={6}>I take half of the size</Cell>
+        <Cell span={6}>me too</Cell>
+      </Layout>
+    </Cell>
   </Layout>
 );
