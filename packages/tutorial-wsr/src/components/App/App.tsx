2fef19ffe856715f9669077639a977f453e9a482
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 12:49:49 +0300

    Add 3 cards in layout

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import { Layout, Cell, Card } from 'wix-style-react';
+import React from "react";
+import { Layout, Cell, Card } from "wix-style-react";
 
 export default () => (
   <Layout>
     <Cell span={6}>
       <Card>
         <Card.Header title="first card" subtitle="nice" />
-        <Card.Divider/>
+        <Card.Divider />
         <Card.Content>some content</Card.Content>
       </Card>
     </Cell>
     <Cell span={6}>
       <Card>
         <Card.Header title="What is Lorem Ipsum?" />
-        <Card.Divider/>
+        <Card.Divider />
         <Card.Content>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s
-          with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
         </Card.Content>
+      </Card>
+    </Cell>
+
+    <Cell span={4}>
+      <Card stretchVertically={true}>
+        <Card.Header title="First card" />
+        <Card.Divider />
+        <Card.Content>Some content.</Card.Content>
+      </Card>
+    </Cell>
+    <Cell span={4}>
+      <Card>
+        <Card.Header title="Second card" subtitle="cool" />
+        <Card.Divider />
+        <Card.Content>Some content.</Card.Content>
+      </Card>
+    </Cell>
+    <Cell span={4}>
+      <Card>
+        <Card.Header title="Third card" subtitle="nice" />
+        <Card.Divider />
+        <Card.Content>Some content.</Card.Content>
       </Card>
     </Cell>
   </Layout>
