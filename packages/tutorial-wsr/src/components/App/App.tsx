91dcf9cc1502337125e88a43059d7f7ea8e1438c
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 14:01:17 +0300

    Add page and layout

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { withTranslation, WithTranslation } from '@wix/wix-i18n-config';
-import s from './App.scss';
+import React from "react";
+import { withTranslation, WithTranslation } from "@wix/wix-i18n-config";
+import { Box, Breadcrumbs, Button, Cell, Layout, Page } from "wix-style-react";
 
 interface AppProps extends WithTranslation {}
 
@@ -9,11 +9,39 @@ class App extends React.Component<AppProps> {
     const { t } = this.props;
 
     return (
-      <div className={s.root}>
-        <h2 className={s.title} data-hook="app-title">
-          {t('app.title', { who: 'Yoshi' })}
-        </h2>
-      </div>
+      <Page>
+        <Page.Header
+          actionsBar={
+            <Box gap="SP2">
+              <Button skin="inverted">Clear</Button>
+              <Button>Submit</Button>
+            </Box>
+          }
+          breadcrumbs={
+            <Breadcrumbs
+              activeId="2"
+              items={[
+                { id: "1", value: "Root Page" },
+                { id: "2", value: "WSR Form" },
+              ]}
+              onClick={() => {}}
+            />
+          }
+          onBackClicked={() => {}}
+          title="WSR Form"
+        />
+        <Page.Content>
+          <Layout>
+            <Cell span={8}>General info</Cell>
+            <Cell span={4}>
+              <Layout>
+                <Cell>Role details</Cell>
+                <Cell>Saved data</Cell>
+              </Layout>
+            </Cell>
+          </Layout>
+        </Page.Content>
+      </Page>
     );
   }
 }
