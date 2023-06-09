9093e5ba121a2c67e553b5d29db1c6d3b2dda792
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 12:51:42 +0300

    Add form field

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Layout, Cell, Card } from 'wix-style-react';
+import { FormField, Input } from 'wix-style-react';
 
 export default () => (
-  <Layout>
-    <Cell span={6}>
-      <Card>
-        <Card.Header title="first card" />
-        <Card.Divider/>
-        <Card.Content>
-          <Layout>
-            <Cell span={4}>first part</Cell>
-            <Cell span={4}>second part</Cell>
-            <Cell span={4}>third part</Cell>
-          </Layout>
-        </Card.Content>
-      </Card>
-    </Cell>
-  </Layout>
+  <FormField
+    label="An input field"
+    required
+    infoContent="Help me fill the field"
+  >
+    <Input />
+  </FormField>
 );
