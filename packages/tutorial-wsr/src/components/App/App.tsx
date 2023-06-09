97ceb5452e10ae3096bdc4c1e17036607fd50683
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 13:14:22 +0300

    Form field exercise

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { FormField, Input } from 'wix-style-react';
+import React from "react";
+import { FormField, Input, Card, Layout, Cell } from "wix-style-react";
 
-export default () => (
-  <FormField
-    label="An input field"
-    required
-    infoContent="Help me fill the field"
-  >
-    <Input />
-  </FormField>
-);
+export default () => {
+  const [charCount, setCharCount] = React.useState(0);
+  const [value, setValue] = React.useState("");
+
+  const handleOnChange = (value: string) => {
+    setValue(value);
+    setCharCount(value.length);
+  };
+
+  return (
+    <Card>
+      <Card.Header title="First card" />
+      <Card.Divider />
+      <Card.Content>
+        <Layout>
+          <Cell span={4}>
+            <FormField
+              label="An input field"
+              required
+              infoContent="Help me fill the field"
+              charCount={5 - charCount}
+            >
+              <Input
+                value={value}
+                onChange={(event) => handleOnChange(event.target.value)}
+              />
+            </FormField>
+          </Cell>
+        </Layout>
+      </Card.Content>
+    </Card>
+  );
+};
