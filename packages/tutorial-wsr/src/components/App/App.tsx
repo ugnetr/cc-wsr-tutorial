946013c93eb57157bc9743ca0f66948b9a978087
Ugne Trakimaite <ugnet@wix.com>
2023-06-09 15:46:29 +0300

    Use WixStyleReactProvider

--- packages/tutorial-wsr/src/components/App/App.tsx
+++ packages/tutorial-wsr/src/components/App/App.tsx
@@ -15,6 +15,7 @@ import {
   Layout,
   Page,
   Text,
+  WixStyleReactProvider,
 } from "wix-style-react";
 import DeleteSmall from "wix-ui-icons-common/DeleteSmall";
 
@@ -72,177 +73,179 @@ const App: FunctionComponent = () => {
   };
 
   return (
-    <Page>
-      <Page.Header
-        actionsBar={
-          <Box gap="SP2">
-            <Button
-              skin="inverted"
-              onClick={handleClearForm}
-              disabled={
-                !formInputData.firstName &&
-                !formInputData.lastName &&
-                !favoriteColor
-              }
-            >
-              Clear
-            </Button>
-            <Button
-              onClick={handleSubmit}
-              disabled={!formInputData.firstName && !formInputData.lastName}
-            >
-              Submit
-            </Button>
-          </Box>
-        }
-        breadcrumbs={
-          <Breadcrumbs
-            activeId="2"
-            items={[
-              { id: "1", value: "Root Page" },
-              { id: "2", value: "WSR Form" },
-            ]}
-          />
-        }
-        title="WSR Form"
-      />
-      <Page.Content>
-        <Layout>
-          <Cell span={8}>
-            <Card>
-              <Card.Header title="General info" />
-              <Card.Divider />
-              <Card.Content>
-                <Layout>
-                  <Cell span={6}>
-                    <FormField label="First name" required>
-                      <Input
-                        name="firstName"
-                        value={formInputData.firstName}
-                        onChange={handleFormInputChange}
-                      />
-                    </FormField>
-                  </Cell>
-                  <Cell span={6}>
-                    <FormField label="Last name" required>
-                      <Input
-                        name="lastName"
-                        value={formInputData.lastName}
-                        onChange={handleFormInputChange}
-                      />
-                    </FormField>
-                  </Cell>
+    <WixStyleReactProvider>
+      <Page>
+        <Page.Header
+          actionsBar={
+            <Box gap="SP2">
+              <Button
+                skin="inverted"
+                onClick={handleClearForm}
+                disabled={
+                  !formInputData.firstName &&
+                  !formInputData.lastName &&
+                  !favoriteColor
+                }
+              >
+                Clear
+              </Button>
+              <Button
+                onClick={handleSubmit}
+                disabled={!formInputData.firstName && !formInputData.lastName}
+              >
+                Submit
+              </Button>
+            </Box>
+          }
+          breadcrumbs={
+            <Breadcrumbs
+              activeId="2"
+              items={[
+                { id: "1", value: "Root Page" },
+                { id: "2", value: "WSR Form" },
+              ]}
+            />
+          }
+          title="WSR Form"
+        />
+        <Page.Content>
+          <Layout>
+            <Cell span={8}>
+              <Card>
+                <Card.Header title="General info" />
+                <Card.Divider />
+                <Card.Content>
+                  <Layout>
+                    <Cell span={6}>
+                      <FormField label="First name" required>
+                        <Input
+                          name="firstName"
+                          value={formInputData.firstName}
+                          onChange={handleFormInputChange}
+                        />
+                      </FormField>
+                    </Cell>
+                    <Cell span={6}>
+                      <FormField label="Last name" required>
+                        <Input
+                          name="lastName"
+                          value={formInputData.lastName}
+                          onChange={handleFormInputChange}
+                        />
+                      </FormField>
+                    </Cell>
 
-                  <Cell>
-                    <Heading size="extraTiny" autoCapitalize="true">
-                      Additional info
-                    </Heading>
-                  </Cell>
-                  <Cell>
-                    <FormField label="Favorite color">
-                      <Layout>
-                        <Cell span={10}>
-                          <Dropdown
-                            placeholder="Choose a color"
-                            options={colorOptions}
-                            selectedId={favoriteColor}
-                            onSelect={(option) =>
-                              setFavoriteColor(option.id as string)
-                            }
-                          />
-                        </Cell>
-                        <Cell span={2}>
-                          <IconButton
-                            priority="secondary"
-                            onClick={handleClearColor}
-                            disabled={!favoriteColor}
-                          >
-                            <DeleteSmall />
-                          </IconButton>
-                        </Cell>
-                      </Layout>
-                    </FormField>
-                  </Cell>
-                  <Cell>
-                    <AddItem disabled>Add New List Item</AddItem>
-                  </Cell>
-                </Layout>
-              </Card.Content>
-            </Card>
-          </Cell>
+                    <Cell>
+                      <Heading size="extraTiny" autoCapitalize="true">
+                        Additional info
+                      </Heading>
+                    </Cell>
+                    <Cell>
+                      <FormField label="Favorite color">
+                        <Layout>
+                          <Cell span={10}>
+                            <Dropdown
+                              placeholder="Choose a color"
+                              options={colorOptions}
+                              selectedId={favoriteColor}
+                              onSelect={(option) =>
+                                setFavoriteColor(option.id as string)
+                              }
+                            />
+                          </Cell>
+                          <Cell span={2}>
+                            <IconButton
+                              priority="secondary"
+                              onClick={handleClearColor}
+                              disabled={!favoriteColor}
+                            >
+                              <DeleteSmall />
+                            </IconButton>
+                          </Cell>
+                        </Layout>
+                      </FormField>
+                    </Cell>
+                    <Cell>
+                      <AddItem disabled>Add New List Item</AddItem>
+                    </Cell>
+                  </Layout>
+                </Card.Content>
+              </Card>
+            </Cell>
 
-          <Cell span={4}>
-            <Layout>
-              <Cell>
-                <Card>
-                  <Card.Header
-                    title="Role details"
-                    suffix={
-                      <Button priority="secondary" disabled>
-                        Edit
-                      </Button>
-                    }
-                  />
-                  <Card.Divider />
-                  <Card.Content>
-                    <Layout>
-                      <Cell>
-                        <Heading size="extraTiny" autoCapitalize="true">
-                          Official title
-                        </Heading>
-                        <Text>Keyboard anihilator</Text>
-                      </Cell>
-                      <Cell>
-                        <Heading size="extraTiny" autoCapitalize="true">
-                          Experience
-                        </Heading>
-                        <Text>It's over nine thousand</Text>
-                      </Cell>
-                    </Layout>
-                  </Card.Content>
-                </Card>
-              </Cell>
-
-              {submittedValues && (
+            <Cell span={4}>
+              <Layout>
                 <Cell>
                   <Card>
-                    <Card.Header title="Saved data" />
+                    <Card.Header
+                      title="Role details"
+                      suffix={
+                        <Button priority="secondary" disabled>
+                          Edit
+                        </Button>
+                      }
+                    />
                     <Card.Divider />
                     <Card.Content>
                       <Layout>
-                        {[
-                          {
-                            title: "First name",
-                            value: submittedValues.firstName,
-                          },
-                          {
-                            title: "Last name",
-                            value: submittedValues.lastName,
-                          },
-                          {
-                            title: "Favorite color",
-                            value: submittedValues.favoriteColor,
-                          },
-                        ]
-                          .filter(({ value }) => !!value)
-                          .map(({ title, value }) => (
-                            <Cell key={title}>
-                              <Heading size="extraTiny" autoCapitalize="true">
-                                {title}
-                              </Heading>
-                              <Text>{value}</Text>
-                            </Cell>
-                          ))}
+                        <Cell>
+                          <Heading size="extraTiny" autoCapitalize="true">
+                            Official title
+                          </Heading>
+                          <Text>Keyboard anihilator</Text>
+                        </Cell>
+                        <Cell>
+                          <Heading size="extraTiny" autoCapitalize="true">
+                            Experience
+                          </Heading>
+                          <Text>It's over nine thousand</Text>
+                        </Cell>
                       </Layout>
                     </Card.Content>
                   </Card>
                 </Cell>
-              )}
-            </Layout>
-          </Cell>
-        </Layout>
-      </Page.Content>
-    </Page>
+
+                {submittedValues && (
+                  <Cell>
+                    <Card>
+                      <Card.Header title="Saved data" />
+                      <Card.Divider />
+                      <Card.Content>
+                        <Layout>
+                          {[
+                            {
+                              title: "First name",
+                              value: submittedValues.firstName,
+                            },
+                            {
+                              title: "Last name",
+                              value: submittedValues.lastName,
+                            },
+                            {
+                              title: "Favorite color",
+                              value: submittedValues.favoriteColor,
+                            },
+                          ]
+                            .filter(({ value }) => !!value)
+                            .map(({ title, value }) => (
+                              <Cell key={title}>
+                                <Heading size="extraTiny" autoCapitalize="true">
+                                  {title}
+                                </Heading>
+                                <Text>{value}</Text>
+                              </Cell>
+                            ))}
+                        </Layout>
+                      </Card.Content>
+                    </Card>
+                  </Cell>
+                )}
+              </Layout>
+            </Cell>
+          </Layout>
+        </Page.Content>
+      </Page>
+    </WixStyleReactProvider>
   );
 };
 
